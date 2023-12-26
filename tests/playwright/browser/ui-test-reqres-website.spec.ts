11c17b6c3a537ff7175a427c68f18654f8bc93c9
unknown <sdet.annguyen@gmail.com>
2023-12-26 16:12:08 +0700

    Eslint

--- tests/playwright/browser/ui-test-reqres-website.spec.ts
+++ tests/playwright/browser/ui-test-reqres-website.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 /**
  * Steps
@@ -7,8 +7,8 @@ import { test, expect } from '@playwright/test';
  * Get the ui response body
  */
 test('has title', async ({ page }) => {
-  await page.goto('https://reqres.in/');
+  await page.goto('https://reqres.in/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Reqres - A hosted REST-API ready to respond to your AJAX/);
-});
+  await expect(page).toHaveTitle(/Reqres - A hosted REST-API ready to respond to your AJAX/)
+})
