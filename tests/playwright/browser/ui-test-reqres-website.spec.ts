8bd3700202507243fe10975f5ff02eab8d3092e5
unknown <sdet.annguyen@gmail.com>
2023-12-26 16:10:14 +0700

    Eslint

--- tests/playwright/browser/ui-test-reqres-website.spec.ts
+++ tests/playwright/browser/ui-test-reqres-website.spec.ts
@@ -8,9 +8,7 @@ import { test, expect } from '@playwright/test';
  */
 test('has title', async ({ page }) => {
   await page.goto('https://reqres.in/');
-  
 
-  
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Reqres - A hosted REST-API ready to respond to your AJAX/);
 });
