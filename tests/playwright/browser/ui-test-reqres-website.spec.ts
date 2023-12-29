b06125da36383c0e6fee0d1cbb0974f3c4d22700
unknown <sdet.annguyen@gmail.com>
2023-12-29 11:26:42 +0700

    Complete ui test

--- tests/playwright/browser/ui-test-reqres-website.spec.ts
+++ tests/playwright/browser/ui-test-reqres-website.spec.ts
@@ -4,11 +4,16 @@ import { test, expect } from '@playwright/test'
  * Steps
  * Access https://reqres.in/
  * Select 'Single user' tab
- * Get the ui response body
+ * Get the ui response ouput
  */
 test('has title', async ({ page }) => {
   await page.goto('https://reqres.in/')
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Reqres - A hosted REST-API ready to respond to your AJAX/)
+
+  await page.getByRole("link",  { name: 'Single user', exact: true }).click()
+
+  const output = await page.getByText('{ "data": { "id": 2, "email').textContent()
+
+  expect(output?.toString()).toContain('janet')
 })
